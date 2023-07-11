16a63ef6d34bfa8e36a7f853d7b411bba457c12c
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2023-07-12 04:01:35 +0530

    adding messege

--- Sum.java
+++ Sum.java
@@ -1,6 +1,6 @@
 class Sum{
     public static void main(String[] args) {
-        System.out.println("hello today 12/07/23");
+        System.out.println("hello time today 12/07/23");
         
     }
 }
